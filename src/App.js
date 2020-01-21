24f817d289baf223259bf33881cdc55c4176d139
Jonathan Bach <jo.bach.s@gmail.com>
2020-01-21 20:37:14 -0300

    primeiro teste react

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './global.css';
+import './App.css';
+
+function App() {
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <label htmlFor="github_username"></label>
+        </form>
+        <h1>Hello world</h1>
+      </aside>
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
